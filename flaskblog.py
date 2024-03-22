1a80478366c2abfddfa68b2eafeea7eecbb939d7
DennysRivera <00026919@uca.edu.sv>
2024-03-21 23:43:11 -0600

    Add: signup and login forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import SignupForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '96e3ab3cdb6e61040b2d79b585d2cf8d'
 
 posts = [
     {
@@ -25,6 +27,27 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        flash(f'Account created successfully', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('signup.html', title='Sign Up', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        #if
+        flash('Logged in successfully', 'success')
+        return redirect(url_for('home'))
+        #else:
+        #    flash('Invalid credentials', 'danger')
+
+    return render_template('login.html', title='Log In', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
